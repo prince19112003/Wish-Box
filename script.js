d1a49d6e5d303dc7e751294270e8ce0b10fd419c
Prince3 <123477475+prince19112003@users.noreply.github.com>
2024-08-04 01:03:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const playButton = document.getElementsByClassName("trigger");
-    const audio = document.getElementById("activeAudio");
-
-    playButton.addEventListener("mousedown", function() {
-        audio.play();
-    });
-
-    playButton.addEventListener("mouseup", function() {
-        audio.pause();
-        audio.currentTime = 0; // Reset audio to the beginning
-    });
-
-    playButton.addEventListener("mouseleave", function() {
-        audio.pause();
-        audio.currentTime = 0; // Reset audio to the beginning if the mouse leaves the button
+    document.addEventListener("contextmenu", function(event) {
+        event.preventDefault();
     });
 });
